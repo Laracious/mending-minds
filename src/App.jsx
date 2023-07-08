f17b201cd4afa4cf1234430ac398a5370592bfc0
Yvan-David <irankundayvan2020@gmail.com>
2023-07-08 21:39:25 +0200

    testing user story

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About/About";
 import Blog from "./components/Blog/Blog";
 import Login from "./components/Login/Login";
 import Sign from "./components/Sign/Sign";
+import Test from './components/Test/Test';
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
 import Notfound from './components/404/Notfound';
@@ -21,6 +22,7 @@ function App() {
         <Route path="/Blog" name="Blog" element={<Blog />} />
         <Route path="/Login" name="Login" element={<Login />} />
         <Route path="/Sign" name="Sign" element={<Sign />} />
+        <Route path="/Test" name="Test" element={<Test />} />
       </Routes>
     </React.Fragment>
   );
